d9da5ae15965dbe06b5a7bb341cbdf7fcdf284c1
Megha <meghacthomas@gmail.com>
2024-04-08 06:20:55 +0530

    user authentication done

---
+++ ACCOUNTS/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import APIException, AuthenticationFailed
+from unittest import mock
+from datetime import timedelta
+class RegisterSerialzer(serializers.Serializer):
+    
+    first_name=serializers.CharField()
+    last_name=serializers.CharField()
+    password=serializers.CharField()
+    username=serializers.CharField()
+    
+    extra_kwargs = {
+            'password':{'write_only':True}
+        }
+    def validate(self, data):
+        
+        if User.objects.filter(username=data['username']).exists():
+           raise serializers.ValidationError('username is taken')
+       
+        return data
+    
+    def create(self, validated_data):
+        user=User.objects.create(first_name=validated_data['first_name'],
+                                last_name=validated_data['last_name'],
+                                username=validated_data['username'],)
+        user.set_password(validated_data['password'])
+        user.save()
+        # instance=self.User(**validated_data)
+        # instance.save()
+        # print(user.password)
+        return validated_data
+        
+        
+        
+class LoginSerialzer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    
+    def validate(self, data):
+        # Your validation logic
+        return data
+    
+    def get_jwt_token(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            return {'message': "Invalid credentials", 'data': {}}
+        
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        
+        # Customize the token expiration time if needed
+        access_token_lifetime = timedelta(hours=24)  # Example: Set access token lifetime to 24 hours
+        
+        # Set token expiration
+        refresh.set_exp(lifetime=access_token_lifetime)
+        access_token_payload = {
+            'token': {
+                'refresh': str(refresh),
+                'access': access_token
+            }
+        }
+        return {'message': "Login successful", 'data': access_token_payload}
+        
+        
--- ACCOUNTS/views.py
+++ ACCOUNTS/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerialzer,LoginSerialzer
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
+
+class Registerview(APIView):
+    
+    def post(self,request):
+        try:
+            
+            data=request.data
+            serializer= RegisterSerialzer(data=data)
+            
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,   
+                    'message':'something went wrong'},status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer.save()
+            
+            return Response({
+                    'data': serializer.data,   
+                    'message':'your account created'},status=status.HTTP_201_CREATED)
+        
+        except Exception as e:
+        
+            return Response({
+               'data': {},   
+               'message':'something wents wrong'},status=status.HTTP_400_BAD_REQUEST) 
+
+class Loginview(APIView): 
+    def post(self,request):
+        try:
+            
+            data=request.data
+            serializer= LoginSerialzer(data=data)
+            
+            if not serializer.is_valid():
+                    return Response({
+                                'data': serializer.errors,   
+                               'message':'somethissng went wrong'},status=status.HTTP_400_BAD_REQUEST) 
+            response=serializer.get_jwt_token(serializer.data)     
+            
+            return Response( response,status=status.HTTP_201_CREATED)
+                       
+                
+        except Exception as e:
+            print(e)
+            return Response({
+               'data': {},   
+               'message':'something wents wrong'},status=status.HTTP_400_BAD_REQUEST)
+
+
+
